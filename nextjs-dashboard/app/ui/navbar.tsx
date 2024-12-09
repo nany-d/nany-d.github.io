41964deb5faacd97c79eb0205c77f749f71d8b58
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-12-08 21:13:16 -0500

    add about section

--- nextjs-dashboard/app/ui/navbar.tsx
+++ nextjs-dashboard/app/ui/navbar.tsx
@@ -5,7 +5,8 @@ export default function Navbar() {
         <ul className="flex items-center text-white justify-end">
             <li className="mr-5 p-2 hover:bg-white hover:text-black"><Link href="/puppeteer" >The Puppeteer</Link></li>
             <li className="mr-5 p-2 hover:bg-white hover:text-black"><Link href="/art">Art</Link></li>
-            <li className="mr-5 p-2 hover:bg-white hover:text-black"><Link href="archive">Archive</Link></li>
+            <li className="mr-5 p-2 hover:bg-white hover:text-black"><Link href="/archive">Archive</Link></li>
+            <li className="mr-5 p-2 hover:bg-white hover:text-black"><Link href="/about">About</Link></li>
             <li className="p-2 hover:bg-white hover:text-black"><Link href="/contact">Contact</Link></li>
         </ul>
     )
