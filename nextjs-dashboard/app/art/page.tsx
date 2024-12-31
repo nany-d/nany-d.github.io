21d9da261480820110d9e6c4bce8b49c499acd99
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-12-31 10:35:21 +0000

    add search to art page

--- nextjs-dashboard/app/art/page.tsx
+++ nextjs-dashboard/app/art/page.tsx
@@ -1,11 +1,27 @@
 import images from "app/images.json"
 import { galleryItem } from "@/app/lib/galleryItem"
 import Gallery from "@/app/ui/Gallery"
+import Search from "../ui/Search"
 
 const imagesData: Array<galleryItem> = images.art
 
-export default function Page() {
+export default async function Page(
+    props: {
+        searchParams?: Promise<{
+          query?: string;
+        }>;
+      }
+) {
+    const searchParams = await props.searchParams
+    const filteredImagesData = imagesData.filter((item, index) => {
+        // Check the name and year
+        return (item.name?.toLocaleLowerCase().includes(searchParams?.query?.toLocaleLowerCase() || "")) || (item.year?.includes(searchParams?.query || ""))
+    })
+    console.log(filteredImagesData)
     return (
-        <Gallery images={imagesData} title="Art" />
+        <>
+        {/* <Search /> */}
+        <Gallery images={filteredImagesData} title="Art" />
+        </>
     )
 }
---
+++ nextjs-dashboard/app/ui/Search.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+export default function Search() {
+    const searchParams = useSearchParams()
+    const pathName = usePathname()
+    const { replace } = useRouter()
+    function handleSearch(term: string) {
+        const urlSearchParams = new URLSearchParams(searchParams)
+        if (term) {
+            urlSearchParams.set("query", term)
+        } else {
+            urlSearchParams.delete("query")
+        }
+        replace(`${pathName}?${urlSearchParams.toString()}`);
+    }
+    return (
+        <div>
+            <input
+                className="text-black"
+                placeholder=""
+                onChange={(e) => handleSearch(e.target.value)}
+                defaultValue={searchParams.get("query")?.toString()}
+            />
+        </div>
+    )
+}
