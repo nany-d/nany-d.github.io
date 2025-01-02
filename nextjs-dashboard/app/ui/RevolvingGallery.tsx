051571343bc94f7ce791a39279344f762d87c588
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2025-01-02 10:47:41 +0000

    tidy up

--- nextjs-dashboard/app/ui/RevolvingGallery.tsx
+++ nextjs-dashboard/app/ui/RevolvingGallery.tsx
@@ -12,7 +12,7 @@ export default function RevolvingGallery({ title, revolvingImages }: galleryProp
     return (
         <div className="flex flex-col mb-20">
             <h1 className="flex flex-wrap justify-start mb-5 font-bold">{title}</h1>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-1 justify-items-center" >
+            <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-1 justify-items-center" >
                 {revolvingImages.map((item, index) => (
                     <RevolvingGalleryItem key={index} images={item.images} refreshInterval={item.refreshInterval}  />
                 ))}
