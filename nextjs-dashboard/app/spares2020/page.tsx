c713dd412c85c7467e19e5d94d827a572e119e6e
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2025-01-02 10:23:14 +0000

    finish collections

---
+++ nextjs-dashboard/app/spares2020/page.tsx
@@ -0,0 +1,13 @@
+import images from "app/images.json"
+import { galleryItem } from "@/app/lib/galleryItem"
+import Gallery from "@/app/ui/Gallery"
+
+const imagesData: Array<galleryItem> = images.spares2020
+
+export default async function Page() {
+    return (
+        <>
+        <Gallery images={imagesData} title="2020 Spares" />
+        </>
+    )
+}
