f32fddb454ef3d34ba59c7fb62559ef5df1adf4c
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2025-01-04 12:19:03 +0000

    add seo meta

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -8,7 +8,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en-UK">
+      <head>
+        <meta name="description" content="Contemporary artwork and game design by Danny Martin | The Puppeteer | Pastel, pen, pencil, digital and mixed media" />
+        <meta name="keywords" content="Danny Haus Martin, Danny, Haus, Martin, Art, The Puppeteer, The Puppeteer video game, Danny House Martin, Danny Martin, Danny Haus, Haus Martin, The Puppeteer, Danny The Puppeteer, Puppeteer, Puppet, Thepuppeteer, Danny Puppeteer, Hard video games, Video games, Dark Souls, Legend of Zelda, Retro games, Pixelart, Pixel art, Retro art, 8bit, 8 bit, 8 bit games, Indie games, Indie game developers, Indie games steam, Blinx the time sweeper, small games, short games, cheap games, small videogames, short videogames, cheap videogames, pastel, pen, digital, mixed media, manga, japanese inpsired, manga inspired, architecture inspired, architecture" />
+        <meta name="author" content="Danny Martin" />
+      </head>
       <body>
         <main className="flex min-h-screen flex-col bg-black sm:px-5">
           <Header />
