a3939653c9a2f15dd29c5bfe0706521656c9802e
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2025-01-04 09:26:13 +0000

    click image to collection

--- nextjs-dashboard/app/ui/GalleryItem.tsx
+++ nextjs-dashboard/app/ui/GalleryItem.tsx
@@ -3,11 +3,14 @@ import Link from "next/link"
 
 export default function GalleryItem({ imgPath, name, year, collectionHref, gridCols, width }: galleryItem) {
     return (
-        <div className={`flex flex-col items-center text-white mx-2.5 mb-5 col-span-1 sm:col-span-${gridCols}`}>
-            <img className={`w-full max-h-[322px] max-w-[${width ?? 250}px]`} src={imgPath} height="auto"/>
+        <div className={`flex flex-col items-center text-white mx-2.5 mb-5 hover:border-[#777] hover:border col-span-1 sm:col-span-${gridCols}`}>
+            {
+                collectionHref ? 
+                <Link href={collectionHref}><img className={`w-full max-h-[322px] max-w-[${width ?? 250}px]`} src={imgPath} height="auto"/></Link> :    
+                <img className={`w-full max-h-[322px] max-w-[${width ?? 250}px]`} src={imgPath} height="auto"/>
+            }
             {name && <p>{name}</p>}
             {year && <p className="italic">{year}</p>}
-            {collectionHref && <Link href={collectionHref}>View Collection</Link>}
         </div>
     )
 }
