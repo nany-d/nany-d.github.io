c861be4f8dbb4a3e690f75bb92bccff517fbf72d
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2025-01-08 16:13:39 +0000

    tweaks

--- app/chaosofchange/page.tsx
+++ app/chaosofchange/page.tsx
@@ -2,12 +2,12 @@ import images from "app/images.json"
 import { galleryItem } from "@/app/lib/galleryItem"
 import Gallery from "@/app/ui/Gallery"
 
-const imagesData: Array<galleryItem> = images.a4s
+const imagesData: Array<galleryItem> = images.chaosofchange
 
 export default async function Page() {
     return (
         <>
-        <Gallery images={imagesData} title="A4s" />
+        <Gallery images={imagesData} title="The Chaos of Change" />
         </>
     )
 }
