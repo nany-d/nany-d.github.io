37e7770432b20838949bc8efc84757056ccc5866
Danny Martin <dannymartin@Dannys-MacBook-Pro.local>
2025-08-29 12:21:32 +0100

    added soul sword test and fixed width

--- app/soulsword/page.tsx
+++ app/soulsword/page.tsx
@@ -5,7 +5,28 @@ import Gallery from "@/app/ui/Gallery"
 export default async function Page() {
     return (
         <>
-            <iframe id="" className={`border-black hover:border-[#777] border`} src="http://localhost:3000/soul_sword/index.html" name="" width="960" height="540"></iframe>
+            <div>
+                <p className={`text-3xl font-bold`}>SOUL SWORD</p>
+                <br/>
+                <br/>
+                <p>
+                    Soul Sword is an experimental videogame prototype that is in its early stages of development. Below is a little traversable demo that changes every time you reload the page!
+                <br/>
+                <br/>
+                    FEATURES: Procedurally generated dungeon maps, hidden lands within lands, MORE TBC
+                <br/>    
+                    NOTE: Only works on PC
+                <br/>
+                <br/>
+                <br/>
+                </p>
+            </div>
+            <div className="justify-items-center" >
+                <iframe id="" className={`border-black hover:border-[#777] border`} src="http://localhost:3000/soul_sword/index.html" name="" width="90%" height="540"></iframe>
+            </div>
+            <div>
+                <br/>
+            </div>
         </>
     )
 }
